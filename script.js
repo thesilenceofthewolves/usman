ac0c8085e7fab283c494ce31ad2f7cfa78ddb72a
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2025-08-08 22:57:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,33 @@
 tsParticles.load("tsparticles", {
-  background: {
-    color: {
-      value: "#ffffff"
-    }
-  },
-  fpsLimit: 60,
-  interactivity: {
-    events: {
-      onHover: {
-        enable: true,
-        mode: "grab"
-      },
-      resize: true
-    },
-    modes: {
-      grab: {
-        distance: 140,
-        line_linked: {
-          opacity: 1
-        }
-      }
-    }
-  },
-  particles: {
-    color: {
-      value: "#999"
-    },
-    links: {
-      color: "#999",
-      distance: 150,
-      enable: true,
-      opacity: 0.5,
-      width: 1
-    },
-    collisions: {
-      enable: false
-    },
-    move: {
-      direction: "none",
-      enable: true,
-      outModes: "bounce",
-      random: false,
-      speed: 1.5,
-      straight: false
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 800
-      },
-      value: 80
-    },
-    opacity: {
-      value: 0.4
-    },
-    shape: {
-      type: "circle"
-    },
-    size: {
-      value: { min: 1, max: 4 }
-    }
-  },
-  detectRetina: true
+  fullScreen: { enable: true, zIndex: -2 },
+  background: { color: "transparent" },
+particles: {
+    number: { value: 60, density: { enable: true, area: 800 } },
+    color: { value: "#BBBBBB" },
+    links: { enable: true, distance: 150, color: "#BBBBBB", opacity: 0.3, width: 1 },
+    move: { enable: true, speed: 0.5, random: true, outModes: "bounce" },
+    shape: { type: "circle" },
+    size: { value: 2 },
+    opacity: { value: 0.6 }
+},
+interactivity: {
+events: {
+      onHover: { enable: true, mode: "grab" },
+      onClick: { enable: false }
+},
+modes: {
+      grab: { distance: 150, links: { opacity: 0.5 } }
+}
+},
+detectRetina: true
 });
+
+const date = new Date();
+document.getElementById('date').textContent = date.toLocaleDateString(undefined, {
+  year: 'numeric', month: 'long', day: 'numeric'
+});
+
+
 
 
 
