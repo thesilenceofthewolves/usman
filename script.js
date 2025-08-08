ef4e0d15615373118ee33c307b9afed6dd61ed8c
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2025-08-08 18:54:02 +0100

    V4 script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/tsparticles@3/tsparticles.bundle.min.js"></script>
+<script src="particles.js"></script>
+<script src="script.js"></script>
 // Auto set the current date in the footer
 const date = new Date();
 const formatted = date.toLocaleDateString(undefined, {
