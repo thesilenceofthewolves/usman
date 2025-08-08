c4c9fe4fcff6baf82b029956ffcc205d5686497f
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2025-08-08 19:06:16 +0100

    V4 script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-//<script src="https://cdn.jsdelivr.net/npm/tsparticles@3/tsparticles.bundle.min.js"></script>
-//<script src="particles.js"></script>
-//<script src="script.js"></script>
 tsParticles.load("tsparticles", {
   fullScreen: {
     enable: true,
     zIndex: -1
-  },
-  background: {
-    color: "#000000"
   },
   particles: {
     number: {
@@ -65,19 +59,3 @@ tsParticles.load("tsparticles", {
     }
   }
 });
-
-
-
-
-// Auto set the current date in the footer
-
-
-const date = new Date();
-const formatted = date.toLocaleDateString(undefined, {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric'
-});
-document.getElementById('date').textContent = formatted;
-
-
