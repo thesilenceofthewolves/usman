b9565b26643c2016f4ff506c8666b7cecceb72f5
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2025-08-08 22:43:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,69 @@
 tsParticles.load("tsparticles", {
-  fullScreen: {
-    enable: true,
-    zIndex: -1
-  },
   background: {
     color: {
-      value: "#000"
-    }
-  },
-  particles: {
-    number: {
-      value: 80,
-      density: {
-        enable: true,
-        area: 800
-      }
-    },
-    color: {
       value: "#ffffff"
-    },
-    links: {
-      enable: true,
-      distance: 150,
-      color: "#ffffff",
-      opacity: 0.4,
-      width: 1
-    },
-    collisions: {
-      enable: false
-    },
-    move: {
-      enable: true,
-      speed: 1,
-      direction: "none",
-      random: false,
-      straight: false,
-      outModes: {
-        default: "bounce"
-      },
-      attract: {
-        enable: false
-      }
-    },
-    opacity: {
-      value: 0.6
-    },
-    shape: {
-      type: "circle"
-    },
-    size: {
-      value: { min: 1, max: 3 }
     }
   },
+  fpsLimit: 60,
   interactivity: {
-    detectsOn: "canvas",
     events: {
       onHover: {
         enable: true,
         mode: "grab"
-      },
-      onClick: {
-        enable: false
       },
       resize: true
     },
     modes: {
       grab: {
         distance: 140,
-        links: {
+        line_linked: {
           opacity: 1
         }
       }
     }
   },
+  particles: {
+    color: {
+      value: "#999"
+    },
+    links: {
+      color: "#999",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1
+    },
+    collisions: {
+      enable: false
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: "bounce",
+      random: false,
+      speed: 1.5,
+      straight: false
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800
+      },
+      value: 80
+    },
+    opacity: {
+      value: 0.4
+    },
+    shape: {
+      type: "circle"
+    },
+    size: {
+      value: { min: 1, max: 4 }
+    }
+  },
   detectRetina: true
 });
+
 
 
 
