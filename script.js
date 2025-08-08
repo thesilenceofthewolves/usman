96ddd4a7aad78c80c35d7b18549a921468c1f0be
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2025-08-08 18:58:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,77 @@
 <script src="https://cdn.jsdelivr.net/npm/tsparticles@3/tsparticles.bundle.min.js"></script>
 <script src="particles.js"></script>
 <script src="script.js"></script>
+tsParticles.load("tsparticles", {
+  fullScreen: {
+    enable: true,
+    zIndex: -1
+  },
+  background: {
+    color: "#000000"
+  },
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        area: 1000
+      }
+    },
+    color: {
+      value: "#999"
+    },
+    links: {
+      enable: true,
+      distance: 200,
+      color: "#999",
+      opacity: 0.5,
+      width: 1
+    },
+    collisions: {
+      enable: false
+    },
+    move: {
+      enable: true,
+      speed: 0.5,
+      direction: "none",
+      random: true,
+      straight: false,
+      outModes: "bounce"
+    },
+    opacity: {
+      value: 0.6
+    },
+    shape: {
+      type: "circle"
+    },
+    size: {
+      value: 2
+    }
+  },
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: "grab"
+      }
+    },
+    modes: {
+      grab: {
+        distance: 140,
+        links: {
+          opacity: 0.7
+        }
+      }
+    }
+  }
+});
+
+
+
+
 // Auto set the current date in the footer
+
+
 const date = new Date();
 const formatted = date.toLocaleDateString(undefined, {
   year: 'numeric',
