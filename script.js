6edbb02bdb411678e8419c2835ad841f0faf0a90
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2025-08-09 02:58:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,73 @@
 tsParticles.load("tsparticles", {
-  fullScreen: { enable: true, zIndex: -1 },
-  background: { color: "#ffffff" },
+  fullScreen: {
+    enable: true,
+    zIndex: -1
+  },
+  background: {
+    color: {
+      value: "#ffffff"
+    }
+  },
   particles: {
-    number: { value: 80, density: { enable: true, area: 800 } },
-    color: { value: "#999999" },
-    links: { enable: true, distance: 120, color: "#cccccc", opacity: 0.4, width: 1 },
-    move: { enable: true, speed: 1 },
-    size: { value: 3 },
-    opacity: { value: 0.5 }
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        area: 800
+      }
+    },
+    color: {
+      value: "#ccc"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5,
+      random: false
+    },
+    size: {
+      value: 3,
+      random: true
+    },
+    links: {
+      enable: true,
+      distance: 150,
+      color: "#aaa",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      outModes: {
+        default: "bounce"
+      }
+    }
   },
   interactivity: {
     events: {
-      onHover: { enable: true, mode: "repulse" },
+      onHover: {
+        enable: true,
+        mode: "grab"
+      },
+      onClick: {
+        enable: true,
+        mode: "push"
+      },
       resize: true
     },
     modes: {
-      repulse: { distance: 100 }
+      grab: {
+        distance: 140,
+        links: {
+          opacity: 0.7
+        }
+      },
+      push: {
+        quantity: 4
+      }
     }
   },
   detectRetina: true
