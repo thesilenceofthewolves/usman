bd064eb04537f286fa16bb6c717db562b97be7c7
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2025-08-09 05:43:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 tsParticles.load("tsparticles", {
-  fullScreen: { enable: true, zIndex: -1 },
-  background: { color: "#ffffff" },
+  fullScreen: {
+    enable: true,
+    zIndex: -1
+  },
+  background: {
+    color: "#ffffff"
+  },
   particles: {
-    number: { value: 80, density: { enable: true, area: 800 } },
-    color: { value: "#999999" },
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        area: 800
+      }
+    },
+    color: {
+      value: "#999999"
+    },
     links: {
       enable: true,
       distance: 120,
@@ -11,17 +24,29 @@ tsParticles.load("tsparticles", {
       opacity: 0.4,
       width: 1
     },
-    move: { enable: true, speed: 1 },
-    size: { value: 3 },
-    opacity: { value: 0.5 }
+    move: {
+      enable: true,
+      speed: 1
+    },
+    size: {
+      value: 3
+    },
+    opacity: {
+      value: 0.5
+    }
   },
   interactivity: {
     events: {
-      onHover: { enable: true, mode: "repulse" },
+      onHover: {
+        enable: true,
+        mode: "repulse"
+      },
       resize: true
     },
     modes: {
-      repulse: { distance: 100 }
+      repulse: {
+        distance: 100
+      }
     }
   },
   detectRetina: true
