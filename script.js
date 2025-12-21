00618577921fb3f2ced99e21840f738293552379
ousman <83393758+thesilenceofthewolves@users.noreply.github.com>
2025-12-21 16:38:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -229,3 +229,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }, 150);
 });
+document.getElementById("theme-toggle").addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+});
